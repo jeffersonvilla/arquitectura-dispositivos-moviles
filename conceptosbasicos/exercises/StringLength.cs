2bf6736a8a35fd1bc23f0126fd086c4e4ab67960
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2024-03-07 21:08:47 -0500

    fix String Length

--- conceptosbasicos/exercises/StringLength.cs
+++ conceptosbasicos/exercises/StringLength.cs
@@ -6,16 +6,11 @@ namespace conceptosbasicos.exercises{
 
             Console.Write("----------> ");
             Console.Write("Ingrese una palabra: ");
-            try{
 
-                String word = Console.ReadLine();
-                
-                Console.Write("----------> ");
-                Console.WriteLine($"Resultado: {word.Length}");   
-
-            }catch(FormatException){
-                Console.WriteLine("Debe ingresar un numero.");
-            }
+            String word = Console.ReadLine();
+            
+            Console.Write("----------> ");
+            Console.WriteLine($"Resultado: {word.Length}");   
         }
     }
 }
