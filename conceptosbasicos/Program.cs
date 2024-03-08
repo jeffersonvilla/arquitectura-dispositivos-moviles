a97a6610ce337d6079f42e7f3c492ca57822277c
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2024-03-07 21:09:01 -0500

    add Vowel Counter

--- conceptosbasicos/Program.cs
+++ conceptosbasicos/Program.cs
@@ -23,6 +23,7 @@ namespace conceptosbasicos{
                 Console.WriteLine("10. String Length");
                 Console.WriteLine("11. Average of Four");
                 Console.WriteLine("12. Smallest of Five");
+                Console.WriteLine("13. Vowel Counter");
                 Console.WriteLine("Ingrese el numero de la opcion que desea usar, (0 para salir):");
                 try{
 
@@ -61,6 +62,8 @@ namespace conceptosbasicos{
                                 case 11: AverageOfFour.run();
                                     break;
                                 case 12: SmallestOfFive.run();
+                                    break;
+                                case 13: VowelCounter.run();
                                     break;
                             }
         
