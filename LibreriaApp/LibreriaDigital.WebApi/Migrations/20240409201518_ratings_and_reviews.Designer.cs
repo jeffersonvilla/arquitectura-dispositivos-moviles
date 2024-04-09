938ef721bebea960f01f7a97c5919a83186fc79f
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2024-04-09 15:19:03 -0500

    add ratings and reviews to the books model

--- LibreriaApp/LibreriaDigital.WebApi/Migrations/20240409201518_ratings_and_reviews.Designer.cs
+++ LibreriaApp/LibreriaDigital.WebApi/Migrations/20240409201518_ratings_and_reviews.Designer.cs
@@ -3,6 +3,7 @@ using LibreriaDigital.WebApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibreriaDigital.WebApi.Migrations
 {
     [DbContext(typeof(LibreriaAppDbContext))]
-    partial class LibreriaAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240409201518_ratings_and_reviews")]
+    partial class ratings_and_reviews
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- LibreriaApp/LibreriaDigital.WebApi/Models/Book.cs
+++ LibreriaApp/LibreriaDigital.WebApi/Models/Book.cs
@@ -6,6 +6,8 @@
         public string Title { get; set; }
         public string Author { get; set; }
         public int PublicationYear { get; set; }
+        public int rating { get; set; }
+        public string review { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
     }
